66df3de36e1384f44386200fd41410ddafc5b130
Gustavo Peixoto <gustavooliveira5676@gmail.com>
2024-03-13 00:47:20 +0000

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro e ${vel} km/h`)
+if (vel >= 60) { //Condicao Simples!
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`diriga sempre usando cinto de seguranca`)
